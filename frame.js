d4aa0b82abeb458fd36b757043684340f58e1141
Oblyv <gregrmcbride@gmail.com>
2026-01-16 15:12:10 -0500

    Add navbar and footer to frame.js

---
+++ frame.js
@@ -0,0 +1,26 @@
+(function () {
+  const navbar = document.createElement("header");
+  navbar.className = "navbar";
+  navbar.innerHTML = `
+    <div class="nav-left">
+      <img src="/res/logo-1.svg" alt="Blocksi" class="logo" />
+    </div>
+
+    <nav class="nav-links">
+      <a href="/gms"><i class="fa-solid fa-gamepad"></i> Games</a>
+      <a href="/aps"><i class="fa-solid fa-layer-group"></i> Apps</a>
+      <a href="/prx"><i class="fa-solid fa-shield"></i> Proxy</a>
+      <a href="/con"><i class="fa-solid fa-envelope"></i> Contact</a>
+    </nav>
+  `;
+
+  document.body.prepend(navbar);
+
+  const footer = document.createElement("footer");
+  footer.className = "site-footer";
+  footer.innerHTML = `
+    <p>© ${new Date().getFullYear()} Blocksi. All rights reserved.</p>
+  `;
+
+  document.body.appendChild(footer);
+})();
